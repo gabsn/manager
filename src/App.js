f476d112ab66d2d2b82f1ddbebc363b91c1f77bc
Gabin Marignier <gabin.marignier@gmail.com>
2016-12-17 18:51:58 +0100

    Initialize repository with boilerplate code

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import firebase from 'firebase'
+import reducers from './reducers'
+import { firebase_config } from './config'
+
+class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp(firebase_config)
+    }
+    
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>
+                        Hello!
+                    </Text>
+                </View>
+            </Provider>
+        )
+    }
+}
+
+export default App
---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+export const firebase_config = {
+    apiKey: "AIzaSyBrprpSirHqaInNMwZA8yD4VrMMuIvII54",
+    authDomain: "manager-b76bc.firebaseapp.com",
+    databaseURL: "https://manager-b76bc.firebaseio.com",
+    storageBucket: "manager-b76bc.appspot.com",
+    messagingSenderId: "1076907231663"
+}
