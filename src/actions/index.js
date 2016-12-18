8c29197dfb56132f628978b2022c137c4da4c3a8
Gabin Marignier <gabin.marignier@gmail.com>
2016-12-18 18:23:29 +0100

    Add Spinner when loading

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 import firebase from 'firebase'
 import { 
     EMAIL_CHANGED,
-    PASSWORD_CHANGED,
-    LOGIN_USER_SUCCESS,
+    LOGIN_USER,
     LOGIN_USER_FAIL,
+    LOGIN_USER_SUCCESS,
+    PASSWORD_CHANGED,
 } from './types'
 
 export const emailChanged = text => {
@@ -22,9 +23,13 @@ export const passwordChanged = text => {
 
 export const loginUser = ({ email, password }) => {
     return dispatch => {
+        dispatch({ type: LOGIN_USER })
+
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(user => loginUserSuccess(dispatch, user))
-            .catch(() => {
+            .catch((error) => {
+                console.log(error)
+
                 firebase.auth().createUserWithEmailAndPassword(email, password)
                     .then(user => loginUserSuccess(dispatch, user))
                     .catch(() => loginUserFail(dispatch))
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Text } from 'react-native'
 import { connect } from 'react-redux'
 import { emailChanged, passwordChanged, loginUser } from '../actions'
-import { Card, CardSection, Input, Button } from './common'
+import { Card, CardSection, Input, Button, Spinner } from './common'
 
 class LoginForm extends Component {
     onEmailChange(text) {
@@ -17,6 +17,19 @@ class LoginForm extends Component {
         const { email, password } = this.props
 
         return this.props.loginUser({ email, password})
+    }
+
+    renderButton() {
+        if (this.props.loading)
+            return (
+                <Spinner />
+            )
+        else
+            return (
+                <Button onPress={this.onButtonPress.bind(this)}>
+                    Login
+                </Button>
+            )
     }
 
     render() {
@@ -46,9 +59,7 @@ class LoginForm extends Component {
                 </Text>
                 
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Login
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         )
@@ -64,9 +75,9 @@ const styles = {
 }
 
 const mapStateToProps = state => {
-    const { email, password, error } = state.auth
+    const { email, password, error, loading } = state.auth
 
-    return { email, password, error }
+    return { email, password, error, loading }
 }
 
 export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm)
