58171bd8fd2b7b9a158ece889dc7fe7370844600
Gabin Marignier <gabin.marignier@gmail.com>
2016-12-18 21:43:32 +0100

    Fix Styling

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Picker, Text } from 'react-native'
 import { connect } from 'react-redux'
 import { employeeUpdate } from '../actions'
 import { Card, CardSection, Input, Button } from './common'
@@ -12,7 +13,7 @@ class EmployeeCreate extends Component {
                         label="Name"
                         placeholder="Jane"
                         value={this.props.name}
-                        onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
+                        onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
                     />
                 </CardSection>
 
@@ -21,11 +22,28 @@ class EmployeeCreate extends Component {
                         label="Phone"
                         placeholder="06 99 99 99 99"
                         value={this.props.phone}
-                        onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
+                        onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
                     />
                 </CardSection>
 
                 <CardSection>
+                    <Text style={pickerTextStyle}>
+                        Shift
+                    </Text>
+
+                    <Picker 
+                        style={{ flex: 1, marginLeft: 60 }}
+                        selectedValue={this.props.shift}
+                        onValueChanged={value => this.props.employeeUpdate({ prop: 'shift', value})}
+                    >
+                        <Picker.Item label="Monday" value="Monday" />
+                        <Picker.Item label="Tuesday" value="Tuesday" />
+                        <Picker.Item label="Wednesday" value="Wednesday" />
+                        <Picker.Item label="Thursday" value="Thursday" />
+                        <Picker.Item label="Friday" value="Friday" />
+                        <Picker.Item label="Saturday" value="Saturday" />
+                        <Picker.Item label="Sunday" value="Sunday" />
+                    </Picker>
                 </CardSection>
 
                 <CardSection>
@@ -36,6 +54,14 @@ class EmployeeCreate extends Component {
             </Card>
         )
     }
+}
+
+const pickerTextStyle = {
+    alignSelf: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 18,
+    paddingLeft: 20,
 }
 
 const mapStateToProps = state => {
