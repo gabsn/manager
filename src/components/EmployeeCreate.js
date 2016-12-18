b8c8d41b7b4e7f95f968740deff6ebafb54ddef4
Gabin Marignier <gabin.marignier@gmail.com>
2016-12-18 21:14:10 +0100

    Add Employee form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { employeeUpdate } from '../actions'
+import { Card, CardSection, Input, Button } from './common'
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <View>
-                <Text>Employee Form</Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Name"
+                        placeholder="Jane"
+                        value={this.props.name}
+                        onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        label="Phone"
+                        planeholder="06 99 99 99 99"
+                        value={this.props.phone}
+                        onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
+                    />
+                </CardSection>
+
+                <CardSection>
+                </CardSection>
+
+                <CardSection>
+                    <Button>
+                        Create
+                    </Button>
+                </CardSection>
+            </Card>
         )
     }
 }
 
-export default EmployeeCreate
+const mapStateToProps = state => {
+    return { name, phone, shift } = state.employeeForm
+}
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate)
