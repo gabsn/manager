db512db71f0c766310b6652069326991e7eaab17
Gabin Marignier <gabin.marignier@gmail.com>
2016-12-19 23:49:12 +0100

    Create Confirm component

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,11 +1,14 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Card, CardSection, Button } from './common'
+import Communications from 'react-native-communications'
+import { Card, CardSection, Button, Confirm } from './common'
 import { employeeUpdate, employeeSave } from '../actions'
 import EmployeeForm from './EmployeeForm'
 
 class EmployeeEdit extends Component {
+    state = { showModal: false }
+
     componentWillMount() {
         _.each(this.props.employee, (value, prop) => {
             this.props.employeeUpdate({ prop, value })
@@ -16,6 +19,7 @@ class EmployeeEdit extends Component {
         return (
             <Card>
                 <EmployeeForm />
+
                 <CardSection>
                     <Button onPress={
                         () => this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid } = this.props)
@@ -23,6 +27,28 @@ class EmployeeEdit extends Component {
                         Save changes
                     </Button>
                 </CardSection>
+
+                <CardSection>
+                    <Button onPress={
+                        () => Communications.text(this.props.phone, `Your upcomming shift is on ${this.props.shift}.`)
+                    }>
+                        Text Schedule
+                    </Button>
+                </CardSection>
+
+                <CardSection>
+                    <Button onPress={() => this.setState({ showModal: true })}>
+                        Fire
+                    </Button>
+                </CardSection>
+
+                <Confirm 
+                    visible={this.state.showModal}
+                    onAccept={() => {}}
+                    onDecline={() => this.setState({ showModal: false })}
+                >
+                    Are you sure you want to fire her/him?
+                </Confirm>
             </Card>
         )
     }
